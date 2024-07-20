2f83be5042b848478b62edbb345207b49c3fde55
Auric Vente <mprops@protonmail.com>
2024-07-19 21:04:20 -0600

    Mods

--- cromulant/config.py
+++ cromulant/config.py
@@ -7,7 +7,7 @@ import appdirs  # type: ignore
 
 class Config:
     title: str = "Cromulant"
-    width: int = 900
+    width: int = 820
     height: int = 900
     max_ants: int = 100
     here: Path
