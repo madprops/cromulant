a9a7936bfcb24b24ef2459e66f2d3d8e6291016d
Auric Vente <mprops@protonmail.com>
2025-12-09 06:46:05 -0600

    Mods

--- cromulant/config.py
+++ cromulant/config.py
@@ -54,7 +54,7 @@ class Config:
     input_caret_color: str = "rgb(18, 18, 18)"
     settings_json: Path
     countries_json: Path
-    filter_debouncer_delay: int = 200
+    filter_debouncer_delay: int = 80
     default_population: int = 100
     merge_goal: int = 10
     manifest_path: Path
